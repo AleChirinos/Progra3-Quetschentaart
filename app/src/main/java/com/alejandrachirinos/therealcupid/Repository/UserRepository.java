f50385872d7eeec54f3da896fcc7cbdcdcf7b8c8
luciano <erickmallea@hotmail.com>
2020-06-18 23:08:55 -0500

    pantalla login2

--- app/src/main/java/com/alejandrachirinos/therealcupid/Repository/UserRepository.java
+++ app/src/main/java/com/alejandrachirinos/therealcupid/Repository/UserRepository.java
@@ -5,7 +5,7 @@ import com.alejandrachirinos.therealcupid.model.User;
 public class UserRepository {
     public User login(String username, String password) {
         if (username.equals("admi") && password.equals("123")) {
-            User user = new User("admi", username,123, "sistemas", "123",null);
+            User user = new User("admi", username,123, "sistemas", "123",null,18);
             return user;
         } else {
             return null;
