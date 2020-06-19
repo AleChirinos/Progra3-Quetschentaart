0b2ca332055e2456f4df4e8140b0b4004551e5e9
alejandra-chirinos <alejandra.chirinos2702@gmail.com>
2020-06-19 17:51:39 -0400

    ScrollView

--- app/src/main/java/com/alejandrachirinos/therealcupid/RegisterActivity.java
+++ app/src/main/java/com/alejandrachirinos/therealcupid/RegisterActivity.java
@@ -199,7 +199,6 @@ public class RegisterActivity extends AppCompatActivity {
                     return;
                 }
 
-
                 Intent loginIntent = new Intent(RegisterActivity.this, LoginActivity.class);
                 startActivity(loginIntent);
             }
