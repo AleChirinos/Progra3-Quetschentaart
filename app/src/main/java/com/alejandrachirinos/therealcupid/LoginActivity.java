cdbe98389dc19125fcb2b17ccb6a13ba4567bc05
luciano <erickmallea@hotmail.com>
2020-06-20 12:08:50 -0500

    pantalla loginFuncional

--- app/src/main/java/com/alejandrachirinos/therealcupid/LoginActivity.java
+++ app/src/main/java/com/alejandrachirinos/therealcupid/LoginActivity.java
@@ -7,11 +7,18 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.alejandrachirinos.therealcupid.Repository.UserRepository;
+import com.alejandrachirinos.therealcupid.model.User;
 
 public class LoginActivity extends AppCompatActivity {
 
     public static String LOG = LoginActivity.class.getName();
     private Button buttonLogin3;
+    private EditText editTextTextPersonName;
+    private EditText editTextTextPassword3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,12 +31,32 @@ public class LoginActivity extends AppCompatActivity {
     }
     private void initViews() {
         buttonLogin3 = findViewById(R.id.loginButton3);
+        editTextTextPersonName = findViewById(R.id.editTextTextPersonName);
+        editTextTextPassword3 = findViewById(R.id.editTextTextPassword3);
 
     }
     private void  addEvents(){
         buttonLogin3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String username = editTextTextPersonName.getText().toString().trim();
+                String password = editTextTextPassword3.getText().toString().trim();
+                if (username.equals("")){
+                    editTextTextPersonName.setError(getString(R.string.error_empty));
+                    return;
+                }
+
+                if (password.equals("")) {
+                    editTextTextPassword3.setError(getString(R.string.error_incorrectValues));
+                    return;
+                }
+                UserRepository repository = new UserRepository();
+                User userLogged = repository.Login(username, password);
+                if (userLogged == null){
+                    Toast.makeText(LoginActivity.this,getString(R.string.error_incorrectValues), Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
                 Intent menuIntent = new Intent(LoginActivity.this, MenuActivity.class);
                 startActivity(menuIntent);
             }
