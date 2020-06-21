67b8ea15e04608cc53bcb1cb5a86c2817cb60619
Denisse <denissebaldiviesopacheco@gmail.com>
2020-06-20 21:31:19 -0400

    SeekBar

--- app/src/main/java/com/alejandrachirinos/therealcupid/WRUInterestedActivity.java
+++ app/src/main/java/com/alejandrachirinos/therealcupid/WRUInterestedActivity.java
@@ -1,13 +1,46 @@
 package com.alejandrachirinos.therealcupid;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.SeekBar;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatSeekBar;
 
 public class WRUInterestedActivity extends AppCompatActivity {
+    public static String LOG = LoginActivity.class.getName();
+    private AppCompatSeekBar demoseekBar;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_interested);
+        initViews();
+        addEvents();
+
+    }
+
+    private void addEvents() {
+        demoseekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean b) {
+                Log.e(LOG,"onProgressChanged"+ progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                Log.e(LOG,"onStopTrackingTouch"+ seekBar.getProgress());
+
+            }
+        });
+    }
+
+    private void initViews() {
+        demoseekBar=findViewById(R.id.demoseekBar);
     }
 }
