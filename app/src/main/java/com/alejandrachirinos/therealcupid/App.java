2d7f1a3c9f24cd762fd10a55947cd447aa3f4241
element222 <fabian.machicao@gmail.com>
2020-06-22 11:37:03 -0400

    first camera, and strings improvements

---
+++ app/src/main/java/com/alejandrachirinos/therealcupid/App.java
@@ -0,0 +1,28 @@
+package com.alejandrachirinos.therealcupid;
+
+import android.app.Application;
+
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+
+import app.horses.camera.CameraManager;
+
+public class App extends Application {
+    private static final String TAG = App.class.getSimpleName();
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        /** Simple Usage (default values) */
+        CameraManager.init(this);
+
+        /** Complete usage */
+        CameraManager.init(
+                new CameraManager.Builder(this)
+                        .setPrimaryColor(R.color.colorPrimary)
+                        .build());
+        ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(this).build();
+        ImageLoader.getInstance().init(config);
+    }
+}
--- app/src/main/java/com/alejandrachirinos/therealcupid/ProfileActivity.java
+++ app/src/main/java/com/alejandrachirinos/therealcupid/ProfileActivity.java
@@ -5,26 +5,35 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 
-
+import com.nostra13.universalimageloader.core.ImageLoader;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatImageButton;
 
 import android.provider.MediaStore;
 import android.util.Log;
+import android.util.SparseIntArray;
+import android.view.Surface;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.view.View.OnClickListener;
 
+import app.horses.camera.CallbackManager;
+import app.horses.camera.CameraManager;
+import app.horses.camera.view.CallbackView;
 
-public class ProfileActivity extends AppCompatActivity {
+
+public class ProfileActivity extends AppCompatActivity implements CallbackView {
     public static String LOG = LoginActivity.class.getName();
+    private static final String TAG = MainActivity.class.getSimpleName();
 
+    private CallbackManager callbackManager = new CallbackManager();
     private AppCompatImageButton buttonCamara;
     private AppCompatImageButton buttonChat;
-    private ImageView img;
+    private ImageView image;
     private Intent intent;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +45,7 @@ public class ProfileActivity extends AppCompatActivity {
 
     private void initViews() {
         buttonCamara = findViewById(R.id.camara);
-        img = findViewById(R.id.imageProfile);
+        image = findViewById(R.id.imageProfile);
     }
 
 
@@ -44,12 +53,42 @@ public class ProfileActivity extends AppCompatActivity {
         buttonCamara.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                abrirCamara();
             }
         });
 
     }
 
+    private void abrirCamara() {
+        CameraManager.openCamera(this);
+        callbackManager.setCallback(this);
+    }
 
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void successCamera(String path) {
+        Log.e(TAG, "successCamera: " + path);
+
+        path = "file:///" + path;
+
+        ImageLoader.getInstance().displayImage(path, image);
+    }
+
+    @Override
+    public void errorCamera() {
+        Log.e(TAG, "errorCamera");
+
+    }
+
+    @Override
+    public void cancelCamera() {
+        Log.e(TAG, "cancelCamera");
+
+    }
 }
