a93f43bdf6fbde6978d27396571c3a6720449b9a
Denisse <denissebaldiviesopacheco@gmail.com>
2020-06-29 17:26:45 -0400

    INTENT email

--- app/src/main/java/com/alejandrachirinos/therealcupid/ContactUsActivity.java
+++ app/src/main/java/com/alejandrachirinos/therealcupid/ContactUsActivity.java
@@ -23,13 +23,53 @@ public class ContactUsActivity extends AppCompatActivity {
         initViews();
         addEvents();
     }
-    public void email(View view){
-        Intent intent=new Intent(Intent.ACTION_SENDTO, Uri.fromParts("mailto","denissebaldiviesopacheco@gmail.com",null));
-
+    public void email1(View view){
+        Intent intent=null, chooser=null;
+        intent =new Intent(Intent.ACTION_SEND);
+        intent.setData(Uri.parse("mailto:"));
+        String[] to={"fabian.machicao@gmail.com"};
+        intent.putExtra(Intent.EXTRA_EMAIL,to);
         intent.putExtra(Intent.EXTRA_SUBJECT,"My problem");
         intent.putExtra(Intent.EXTRA_TEXT,"Hola! Quisiera hacer una pregunta, ");
-        intent.setType("text/plain");
-        startActivity(intent);
+        intent.setType("message/rfc822");
+        chooser=Intent.createChooser(intent,"send email");
+        startActivity(chooser);
+    }
+    public void email2(View view){
+        Intent intent=null, chooser=null;
+        intent =new Intent(Intent.ACTION_SEND);
+        intent.setData(Uri.parse("mailto:"));
+        String[] to={"alejandra.chirinos2702@gmail.com"};
+        intent.putExtra(Intent.EXTRA_EMAIL,to);
+        intent.putExtra(Intent.EXTRA_SUBJECT,"My problem");
+        intent.putExtra(Intent.EXTRA_TEXT,"Hola! Quisiera hacer una pregunta, ");
+        intent.setType("message/rfc822");
+        chooser=Intent.createChooser(intent,"send email");
+        startActivity(chooser);
+    }
+    public void email3(View view){
+        Intent intent=null, chooser=null;
+        intent =new Intent(Intent.ACTION_SEND);
+        intent.setData(Uri.parse("mailto:"));
+        String[] to={"erickmallea1234@gmail.com"};
+        intent.putExtra(Intent.EXTRA_EMAIL,to);
+        intent.putExtra(Intent.EXTRA_SUBJECT,"My problem");
+        intent.putExtra(Intent.EXTRA_TEXT,"Hola! Quisiera hacer una pregunta, ");
+        intent.setType("message/rfc822");
+        chooser=Intent.createChooser(intent,"send email");
+        startActivity(chooser);
+    }
+    public void email4(View view){
+        Intent intent=null, chooser=null;
+        intent =new Intent(Intent.ACTION_SEND);
+        intent.setData(Uri.parse("mailto:"));
+        String[] to={"denissebaldiviesopacheco@gmail.com"};
+        intent.putExtra(Intent.EXTRA_EMAIL,to);
+        intent.putExtra(Intent.EXTRA_SUBJECT,"My problem");
+        intent.putExtra(Intent.EXTRA_TEXT,"Hola! Quisiera hacer una pregunta, ");
+        intent.setType("message/rfc822");
+        chooser=Intent.createChooser(intent,"send email");
+        startActivity(chooser);
     }
     private void initViews() {
         logOff = findViewById(R.id.logOffTextView);
